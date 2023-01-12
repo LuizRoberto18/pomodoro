70756c80c5083dd49a37ae297f62006f6b5ae433
Luiz Roberto <luiz.santos@carajas.net.br>
2023-01-12 12:02:17 -0300

    criando pomodoros.store, componente cronometro, implementando fuções para gerennciar o cronometro dinamicamente, e introduzindo provider para notificar as ações do mobx na tela

--- lib/components/entrada_tempo.dart
+++ lib/components/entrada_tempo.dart
@@ -1,56 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../store/pomodoro.store.dart';
 
 class EntradaTempo extends StatelessWidget {
   final String titulo;
   final int valor;
+  final void Function()? inc;
+  final void Function()? dec;
 
   const EntradaTempo({
     Key? key,
     required this.titulo,
     required this.valor,
+    this.inc,
+    this.dec,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Text(
-          this.titulo,
-          style: TextStyle(
+          titulo,
+          style: const TextStyle(
             fontSize: 25,
           ),
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: dec,
               style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(15),
-                primary: Colors.red,
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                backgroundColor: store.estaTrabalhando() ? Colors.red : Colors.green,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.arrow_downward,
                 color: Colors.white,
               ),
             ),
             Text(
-              "${this.valor} min",
-              style: TextStyle(
+              "$valor min",
+              style: const TextStyle(
                 fontSize: 18,
               ),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: inc,
               style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(15),
-                primary: Colors.red,
+
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                backgroundColor: store.estaTrabalhando() ? Colors.red : Colors.green,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.arrow_upward,
                 color: Colors.white,
               ),
--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/components/cronometro.dart';
 import 'package:pomodoro/components/entrada_tempo.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -16,18 +21,24 @@ class Pomodoro extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                EntradaTempo(
-                  titulo: "Trabalho",
-                  valor: 25,
-                ),
-                EntradaTempo(
-                  titulo: "Descanço",
-                  valor: 5,
-                ),
-              ],
+            child: Observer(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  EntradaTempo(
+                    titulo: "Trabalho",
+                    valor: store.tempoTrabalho,
+                    inc: store.incrementarTempoTrabalho,
+                    dec: store.decrementarTempoTrabalho,
+                  ),
+                  EntradaTempo(
+                    titulo: "Descanço",
+                    valor: store.tempoDescanso,
+                    inc: store.incrementarTempoDescanso,
+                    dec: store.decrementarTempoDescanso,
+                  ),
+                ],
+              ),
             ),
           ),
         ],
